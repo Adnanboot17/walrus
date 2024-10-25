f717caf7dc4cc94ad10dd60f793f18d6cdd7366c
Adnanboot17 <nismalaningnsih@gmail.com>
2024-10-25 14:12:25 +0700

    Update accounts.js

--- app/accounts/accounts/accounts.js
+++ app/accounts/accounts/accounts.js
@@ -1,33 +1,3 @@
-/**
- * Private key list file
- * write your private key here like this
-  export const privateKey = [
-    "suiprivkey1qpq35yxg9je8ttdruhmcquukrw358yqz44lw9z0eu250zh7j0uuczxxcuv8",
-  "suiprivkey1qr8g3yzea8pru4wtlttan0p22dwwx2m42jy9gkc05mmv8nnyj8jhzwj4hxr",
-   "suiprivkey1qqsqvg9t4qc5mp26d6mfylvrvzp5hejy8fcclu2mr9fjx7gyf8uyypcevp3",
- "suiprivkey1qqagvt2325gv5cuxmqsfz60cq4kkwe2zefakzns8hpf6qzvdjmtk2vhs8mk",
- "suiprivkey1qr62c9549crxr4dqwv5h5y25qxatc3977umhdmu7xz2t5293uvzg6k2a9y8",
-  "suiprivkey1qrmjjcg5gpcz965hp8n36jtmnnjlvmug52u5s380dzz2rg0d48g8k7659ju",
- "suiprivkey1qpcldml7f3ytfqq3h4je6lngwwtmxhds6xpfmeaurdh9dhs7rwnjz4g5ptg",
- "suiprivkey1qrxjyc2x6le3eus4f4y90kvx5n8n3r5725r3yyxjxq06f8853fvgulefu2w",
- "suiprivkey1qzm3m59ala4j25gjyj4u3wwxsz0g77k4pagg446re9eskm6wmf4a66suh3j",
-  "suiprivkey1qqlxgul7w47n25j2r5t3v3pxdcckwlx833f3me59vatvyn9ezcsfg4rk3aw",
- "suiprivkey1qrmck096ty6dzg72w0ytf9mmaa9j40wyk0sxn3xdshc52299nnhxwushgv6",
-"suiprivkey1qzrslydh2ny5cs36ev368ncypdgf338sfawupz7dcvkfvgz4f8uukrnnggv",
- "suiprivkey1qrcn0l7u8jkratcrwtrdqe0wv9m3u7mprm3hxzeg90u73st2l4xgvu87m6u",
-  "suiprivkey1qrcn0l7u8jkratcrwtrdqe0wv9m3u7mprm3hxzeg90u73st2l4xgvu87m6u",
- "suiprivkey1qp39g90tf869gsr86slh66st0jaant77h4j59zs2465cmnsd08szspfnlfg",
-  "suiprivkey1qp39g90tf869gsr86slh66st0jaant77h4j59zs2465cmnsd08szspfnlfg",
- "suiprivkey1qr0lh0p2vp38nax0yk67c8f82uhlpznfetr50w6re7pm0fxtek6pvfayw5r",
-  "suiprivkey1qqc5xv9s5e6lupgkcg3utaecn5fh4y60t8yxzxxl2j9jx9jp3h3axwm7xdf",
- "suiprivkey1qpcuamz4xr7m2dqgawk7uc3747t8pgftr57sck2965g64g4vnhnm5dp6yuz",
- "suiprivkey1qrwtultep5dg333fe96scn3fp6q5jk8k9ekly8dlvzwm205g3ekc2js47rw",
- "suiprivkey1qp0xqjejj5fwax0tumxm64x00cyzwtlr40jfc9q7menzet89xjg2zvmwmmz",
-  "suiprivkey1qrmn8tzx744sj5ut20s345g3mq2jxarqt5rfzxk0pc09xdt2gyu7z276lvy",
-
-  ];
- */
-
 export const privateKey = [
  "pklu",
 
